90bddce027eabbb53393ad471b8c085ee7682f44
akaBaytar <burak.bilgili@icloud.com>
2023-11-03 15:59:30 +0300

    'svgr' installed to import SVGs & 'navbar' created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Fragment } from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
+import Navbar from './components/Navbar';
+
 import Explore from './pages/Explore';
-import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
@@ -20,6 +22,7 @@ function App() {
           <Route path='/sign-up' element={<SignUp/>}/>
           <Route path='/forgot-password' element={<ForgotPassword/>}/>
         </Routes>
+        <Navbar />
       </Router>
     </Fragment>
   );
