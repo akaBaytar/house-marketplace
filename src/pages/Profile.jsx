4e8378fac8f8f98a3a54bb032523d49ce0eecbec
akaBaytar <burak.bilgili@icloud.com>
2023-11-03 20:39:07 +0300

    added change name functionality and user infos

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 
-import { getAuth } from 'firebase/auth';
+import { doc, updateDoc } from 'firebase/firestore';
+import { getAuth, updateProfile } from 'firebase/auth';
+
+import { database } from '../../firebase.config';
+import { toast } from 'react-toastify';
 
 const Profile = () => {
   const auth = getAuth();
+
+  const [changeDetails, setChangeDetails] = useState(false);
+
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -15,13 +22,43 @@ const Profile = () => {
   // destructuring form data
   const { name, email } = formData;
 
-  // handler function
+  // handler functions
   const onLogout = () => {
     // logout
     auth.signOut();
 
     // redirecting the user to homepage
     navigate('/');
+  };
+
+  const onClickHandler = () => {
+    changeDetails && onSubmitHandler();
+    setChangeDetails((prevState) => !prevState);
+  };
+
+  const onSubmitHandler = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // update display name in firebase
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        //update display name in firestore
+        const userReference = doc(database, 'users', auth.currentUser.uid);
+        await updateDoc(userReference, { name });
+      }
+    } catch (error) {
+      toast.error('Profile details could not be updated.');
+    }
+  };
+
+  // changing name and/or email infos
+  const onChangeHandler = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
   };
 
   return (
@@ -32,6 +69,36 @@ const Profile = () => {
           Logout
         </button>
       </header>
+      <main>
+        <div className='profileDetailsHeader'>
+          <p className='personalDetailsText'>Personal Details</p>
+          <p className='changePersonalDetails' onClick={onClickHandler}>
+            {changeDetails ? 'Done' : 'Change'}
+          </p>
+        </div>
+        <div className='profileCard'>
+          <form>
+            <input
+              type='text'
+              id='name'
+              className={!changeDetails ? 'profileName' : 'profileNameActive'}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChangeHandler}
+              autoComplete='off'
+            />
+            <input
+              type='text'
+              id='email'
+              className='profileEmail'
+              disabled='disabled'
+              value={email}
+              onChange={onChangeHandler}
+              autoComplete='off'
+            />
+          </form>
+        </div>
+      </main>
     </div>
   );
 };
